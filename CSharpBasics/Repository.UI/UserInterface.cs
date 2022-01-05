23dcf363853a83efc3cc643119a7900a27499626
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2022-01-05 16:36:07 -0500

    Completed UI

--- CSharpBasics/Repository.UI/UserInterface.cs
+++ CSharpBasics/Repository.UI/UserInterface.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Repository.Library;
 
 namespace Repository.UI
@@ -19,7 +20,7 @@ namespace Repository.UI
             while (isRunning)
             {
                 //Add this later
-                Console.Clear();
+                // Console.Clear();
 
 
                 Console.WriteLine(
@@ -72,18 +73,166 @@ namespace Repository.UI
         //Add new
         private void CreateNewContent()
         {
+            Console.Clear();
 
+            StreamingContent content = new StreamingContent();
+
+            Console.Write("Please enter a title: ");
+            content.Title = Console.ReadLine();
+
+            Console.Write("Please enter a description: ");
+            content.Description = Console.ReadLine();
+
+            Console.Write("Please enter a rating 1-10");
+            content.StarRating = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(
+                "Select a Maturity Rating: \n" +
+                "1. G \n" +
+                "2. PG \n" +
+                "3. PG-13 \n" +
+                "4. R \n" +
+                "5. NC-17 \n"
+                );
+
+            string maturityResponse = Console.ReadLine();
+            switch (maturityResponse)
+            {
+                case "1":
+                    content.MaturityRating = MaturityRating.G;
+                    break;
+
+                case "2":
+                    content.MaturityRating = MaturityRating.PG;
+                    break;
+                case "3":
+                    content.MaturityRating = MaturityRating.PG_13;
+                    break;
+                case "4":
+                    content.MaturityRating = MaturityRating.R;
+                    break;
+                case "5":
+                    content.MaturityRating = MaturityRating.NC_17;
+                    break;
+            }
+
+            _repo.AddContentToDirectory(content);
         }
         //Add Movie/Show?
 
         //Get all
+        private void ShowAllContent()
+        {
+            Console.Clear();
+            List<StreamingContent> listOfContent = _repo.GetContents();//could do movie/show
+
+            //can print to the console the collection initally if desired.
+            foreach (StreamingContent content in listOfContent)
+            {
+                //Initially can write out display info here
+                DisplayContent(content);
+
+            }
+            System.Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        //Helper method
+        private void DisplayContent(StreamingContent content)
+        {
+            Console.WriteLine(
+                $"Title: {content.Title} \n" +
+                $"Description: {content.Description}\n" +
+                $"Stars: {content.MaturityRating} \n" +
+                $"Maturity Rating \n" +
+                $"Family Friendly \n"
+            );
+        }
 
         //Get by title
+        private void ShowContentByTitle()
+        {
+            Console.Clear();
+            System.Console.WriteLine("What title are you looking for?");
+
+            string targetTitle = Console.ReadLine();
+
+            StreamingContent content = _repo.GetContentByTitle(targetTitle);
+
+            if (content != null)
+            {
+                DisplayContent(content);
+            }
+            else
+            {
+                Console.WriteLine("Title not found");
+            }
+
+            Continue();
+        }
+
+        private static void Continue()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        //At this point might want to go ahead and add a seed method for testing.
 
         //Remove
+        private void RemoveContentFromRepo()
+        {
+            //Ask class what the best way of removing would be? might get suggestion for remove by title. But going to lean towards showing what we have first and then making a selection.
 
+            Console.Clear();
+
+            System.Console.WriteLine("Which item would you like to remove?");
+
+            List<StreamingContent> contentList = _repo.GetContents();
+            //can add this after getting initial display up and running
+            int count = 0;
+
+            foreach(StreamingContent content in contentList)
+            {
+                count++;
+                Console.WriteLine($"{count}. {content.Title}");
+            }
+
+            int contentSelection = int.Parse(Console.ReadLine());
+            int targetIndex = contentSelection - 1;
+
+            //check for valid selection(depending on time can leave as challenge)
+            if(targetIndex >= 0 && targetIndex < contentList.Count)
+            {
+                StreamingContent targetContent = contentList[targetIndex];
+
+                if(_repo.DeleteExistingContent(targetContent))
+                {
+                    System.Console.WriteLine($"{targetContent.Title} was successfully removed");
+                }
+                else//If content fails to be removed.
+                {
+                    System.Console.WriteLine("Something went wrong!");
+                }
+
+            }
+            else//If given invalid selection
+            {
+                System.Console.WriteLine("Invalid Selction");
+            }
+            Continue();
+        }
         //Update?
+        
 
+
+        //Seed
+        private void SeedContentList()
+        {
+            StreamingContent alien = new StreamingContent("Alien", "Deep Space Sci-Fi Horror Movie", 8.4, MaturityRating.R);
+            StreamingContent theTrumanShow = new StreamingContent("The Truman Show", "The Real Reality TV", 9.3, MaturityRating.PG);
+            StreamingContent insideOut = new StreamingContent("Inside Out", "Do you ever look at someone and wonder \"What is going on inside their head?\"", 8.7, MaturityRating.G);
+        }
 
 
 
